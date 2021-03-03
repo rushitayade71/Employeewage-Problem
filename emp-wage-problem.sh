403854b527a17e0023b447c9a33c01534ac4ea76
rushitayade71 <rushitayade71@gmail.com>
2021-03-03 16:42:57 +0530

    Check Empolyee Is Present Or Absent

--- emp-wage-problem.sh
+++ emp-wage-problem.sh
@@ -1,3 +1,12 @@
 #!/bin/bash -x
 echo "Welcome To Employee Wage Problems"
 
+Present=1;
+randomCheck=$((RANDOM%2));
+
+if [ $Present -eq $randomCheck ]
+then
+        echo "Employee is Present";
+else
+        echo "Employee is Abscent";
+fi
