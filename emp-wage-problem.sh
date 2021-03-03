773619eaa376636dbb3d5f9b23169a2459ef5e6d
rushitayade71 <rushitayade71@gmail.com>
2021-03-03 17:20:42 +0530

    Calculate Daily Employee Wage

--- emp-wage-problem.sh
+++ emp-wage-problem.sh
@@ -5,8 +5,11 @@ Present=1;
 randomCheck=$((RANDOM%2));
 
 if [ $Present -eq $randomCheck ]
-then
-        echo "Employee is Present";
-else
-        echo "Employee is Abscent";
+then 
+         empRatePerHr=20;
+         empHrs=8;
+         salary=$(( $empHrs*$empRatePerHr )) 
+   else
+      salary=0;
 fi
+echo "Daily Wage of an Employee= " $salary
