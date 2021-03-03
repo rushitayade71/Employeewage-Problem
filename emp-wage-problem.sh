1ddad81a308bbfc8fc2be7dbc93f60c5069aa0a7
rushitayade71 <rushitayade71@gmail.com>
2021-03-03 17:54:31 +0530

    Calculating Employee Wages For A Month

--- emp-wage-problem.sh
+++ emp-wage-problem.sh
@@ -1,23 +1,38 @@
 #!/bin/bash -x
 echo "Welcome To Employee Wage Problems"
 
-isPartTime=1
-isFullTime=2
-empRatePerHr=20
-empCheck=$((RANDOM%3))
+IS_PART_TIME=1;
+IS_FULL_TIME=2;
+MAX_HRS_IN_MONTH=4;
+EMP_RATE_PER_HR=20;
+NUM_WORKING_DAYS=20;
 
-case $empCheck in
-          $isFullTime)
-                    empHrs=8
-                        ;;
-                  $isPartTime)
-                        empHrs=4
-                            ;;
-               *)
-               empHrs=0
+totalEmpHr=0;
+totalWorkingDays=0;
+
+function getWorkingHours(){
+     case $1 in
+         $IS_FULL_TIME)
+          workHours=8
                    ;;
+        $IS_PART_TIME)
+             workHours=4
+                   ;;
+      *)
+  workHours=0
+      ;;
 esac
+echo $workHours
+}
 
-  salary=$(($empHrs*$empRatePerHr)) 
+while [[ $totalWorkHours -lt $MAX_HRS_IN_MONTH && $totalWorkingDays -lt $NUM_WORKING_DAYS ]] 
+do
+  (( totalWorkingDays++ ))
+        workHours=$( getWorkingHours $((RANDOM%3)) )
+        totalWorkHours=$(($totalWorkHours+$workHours))
 
-echo "Salary Of Employee= "$salary
+ done
+
+totalSalary=$(($totalWorkHours*$EMP_RATE_PER_HR));
+
+echo "salary of an Employee= "$totalSalary
